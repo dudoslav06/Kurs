23ea0931175e1f31f345c8e12ca87c7043c5e3d8
Łukasz Duda <41270249+dudoslav06@users.noreply.github.com>
2021-04-28 21:10:45 +0200

    Add files via upload

---
+++ Kurs - midadvance/1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek adi = new Czlowiek();
+            adi.imie = "Adrian";
+            adi.Wiek = -20;
+            Console.WriteLine($"Jestem {adi.imie} mam lat {adi.Wiek}.");
+            Console.ReadKey();
+        }
+
+        
+
+        class Czlowiek
+        {
+            public string imie;
+            //private int wiek;
+            //public int wiek { get; set; } // to jest prymitywne properties ponizej dajemy pelne 
+
+            private int wiek;
+
+            public static int Wiek
+            {
+                get { return wiek; }
+                set 
+                {
+                    if (value >= 0 && value <= 200)
+                        wiek = value;
+                    else
+                        wiek = 0;
+                }
+            }
+
+        }
+
+    }
+    //class Czlowiek // mozna w ten sposób wykonac getter setter, tak sie robi, np w Javie, ale
+    //w c# jest to dużo bardziej rozbudowane i ułatwione jednocześnie, jak wyżej^
+    //{
+    //    public string imie;
+    //    private int wiek;
+
+    //    public int getWiek()
+    //    {
+    //        return wiek;
+    //    }
+
+    //    public void setWiek(int w)
+    //    {
+    //        wiek = w;
+    //    }
+        
+    //}
+
+}
---
+++ Kurs - midadvance/Rzutowanie i konwersja typow/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj dzien swoich urodzin:");
+            int dayOfBirthDate = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj miesiac swoich urodzin:");
+            int mounthOfBirthDate = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj rok swoich urodzin:");
+            int yearOfBirthDate = int.Parse(Console.ReadLine());
+
+
+            DateTime dateOfBirth = new DateTime(yearOfBirthDate, mounthOfBirthDate, dayOfBirthDate);
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+
+
+
+            Console.WriteLine(timeSpan);
+
+            
+
+
+
+
+
+
+            //byte byteValue = 100;
+            //int intValue = byteValue;
+            //byte byteValue2 = (byte)intValue;
+            //string userInput = Console.ReadLine();
+            //int yearOfBirth;
+            //if (int.TryParse(userInput, out yearOfBirth))
+            //{
+            //    int age = DateTime.Now.Year - yearOfBirth;
+
+            //    Console.WriteLine("You are" + age);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Incorrect value");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
